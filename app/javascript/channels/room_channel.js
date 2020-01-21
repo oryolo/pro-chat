aef289c273fed1e5aa40375bab9fb6cc31cb6fee
Olga Orel <olga.detterer@gmail.com>
2020-01-21 15:07:51 -0800

    Fix CSS styling

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,24 +1,3 @@
-// import consumer from "./consumer"
-
-// consumer.subscriptions.create("RoomChannel", {
-//   connected() {
-//     // Called when the subscription is ready for use on the server
-//   },
-
-//   disconnected() {
-//     // Called when the subscription has been terminated by the server
-//   },
-
-//   received(data) {
-//     // Called when there's incoming data on the websocket for this channel
-//     var content = messageTemplate.children().clone(true, true);
-//     content.find('[data-role="message-text"]').text(data.message);
-//     content.find('[data-role="message-date"]').text(data.updated_at);
-//     $element.append(content);
-//     $element.animate({ scrollTop: $element.prop("scrollHeight") }, 1000);
-//   }
-// });
-
 $(function () {
   $('[data-channel-subscribe="room"]').each(function (index, element) {
     var $element = $(element),
